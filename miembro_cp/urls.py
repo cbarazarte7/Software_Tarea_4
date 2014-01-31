e1396951f15a843f71ff6ffc8bf8a2e095fdaa33
Alejandro <alexgarbi10@gmail.com>
2014-01-30 20:52:35 -0430

    Sin miembro cp

--- miembro_cp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import patterns, url
-
-from autor import views
-
-urlpatterns = patterns('',
-		url(r'^$', views.index, name='index'),
-    url(r'results/$', views.results, name='results'),
-    url(r'create/$', views.nuevo_miembro_cp)
-)
